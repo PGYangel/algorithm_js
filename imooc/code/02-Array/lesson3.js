6ed8248e4786f997ea8fd6c163a543ea700d8f95
叶荣 <rong.ye@patpat.com>
2022-01-27 16:26:12 +0800

    种花问题

--- imooc/code/02-Array/lesson3.js
+++ imooc/code/02-Array/lesson3.js
@@ -19,9 +19,38 @@
 */
 
 export default (flowerbed, n) => {
+    /* 
+    解题思路，先原来花坛前后各加上一个0，判断3个一组数据是否都是000，只有这种情况才一定能种上。前后加0是为了方便统一格式判断。
+    加上后数组就成[0,1,0,0,0,0,0,1,0]，这样不用考虑边界判断问题
+    种上一棵后需要用新的数组重新计算000的情况，不然会遇见1000001中间五个的情况，这种情况只能种2，不重新计算会得到3个000情况。
+    */
     let result = true
-    console.log(flowerbed)
-    console.log(n)
-
+    let num = n
+    // 前后加上0
+    flowerbed.unshift(0)
+    flowerbed.push(0)
+    function planting(arr, index) {
+        // 输入的三个都为0才对num减少，并对原数组对应种植数据改为1
+        if (!arr.includes(1)) {
+            num--
+            flowerbed[index] = 1
+        }
+    }
+    for (let i = 0, il = flowerbed.length - 2; i < il; i++) {
+        let arr = flowerbed.slice(i, i + 3)
+        planting(arr, i + 1)
+    }
+    if (num === 0) {
+        result = true
+    } else {
+        result = false
+    }
     return result
 }
+
+/* 
+知识点
+Array.prototype.slice：可提取字符串的某个部分，并以新的字符串返回被提取的部分
+
+
+*/
--- imooc/test/02-Array/lesson3.test.js
+++ imooc/test/02-Array/lesson3.test.js
@@ -1,9 +1,21 @@
 import lesson3 from '../../code/02-Array/lesson3'
 
-test('lesson3',()=>{
-    expect(lesson3([1,0,0,0,1],1)).toBe(true)
+test('lesson3', () => {
+    expect(lesson3([1, 0, 0, 0, 1], 1)).toBe(true)
 })
 
-// test('lesson3',()=>{
-//     expect(lesson3([1,0,0,0,1],2)).toBe(false)
-// })
+test('lesson3', () => {
+    expect(lesson3([1, 0, 0, 0, 1], 2)).toBe(false)
+})
+
+test('lesson3', () => {
+    expect(lesson3([1, 0, 0, 0, 0, 0, 1], 3)).toBe(false)
+})
+
+test('lesson3', () => {
+    expect(lesson3([0, 1, 0, 0, 0, 0, 0, 1, 0], 3)).toBe(false)
+})
+
+test('lesson3', () => {
+    expect(lesson3([0, 0, 1, 0, 0, 0, 1], 2)).toBe(true)
+})
