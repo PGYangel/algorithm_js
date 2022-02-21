e6805494ab2fb19297c3fa4ea95bd7c4fb6ca5cf
叶荣 <rong.ye@patpat.com>
2022-02-21 17:51:16 +0800

    格雷编码

--- imooc/test/02-Array/lesson4.test.js
+++ imooc/test/02-Array/lesson4.test.js
@@ -1,5 +1,9 @@
 import lesson4 from '../../code/02-Array/lesson4'
 
-// test('lesson4', () => {
-//     expect(lesson4([1, 0, 0, 0, 1], 1)).toBe(true)
-// })
+test('lesson4', () => {
+    expect(lesson4(2)).toEqual(['00','01','11','10'])
+})
+
+test('lesson4', () => {
+    expect(lesson4(3)).toEqual(['000','001','011','010','110','111','101','100'])
+})
---
+++ imooc/test/03-RegExp/lesson1.test.js
@@ -0,0 +1,5 @@
+import lesson1 from '../../code/03-RegExp/lesson1'
+
+// test('lesson1', () => {
+//     expect(lesson1(2)).toEqual(['00','01','11','10'])
+// })
