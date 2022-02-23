f675dbb0adb92f02cedd51486b16b44886ea156b
PGY_angel <241761025@qq.com>
2022-02-23 20:46:22 +0800

    冒泡排序

--- imooc/code/04-sort/lesson1.js
+++ imooc/code/04-sort/lesson1.js
@@ -1,3 +1,18 @@
-export default () => {
-
+/* 
+冒泡排序
+外层循环从大到小逐渐缩小排序范围
+内层循环从小到大逐渐将最大值往右靠
+*/
+export default (arr) => {
+    let temp = null
+    for (let i = arr.length - 1; i >= 0; i--) {
+        for (let j = 0; j <= i; j++) {
+            if (arr[j] >= arr[j + 1]) {
+                temp = arr[j + 1]
+                arr[j + 1] = arr[j]
+                arr[j] = temp
+            }
+        }
+    }
+    return arr
 }
--- imooc/test/04-sort/lesson1.test.js
+++ imooc/test/04-sort/lesson1.test.js
@@ -1,5 +1,9 @@
 import lesson1 from '../../code/04-sort/lesson1'
 
-// test('lesson1', () => {
-//     expect(lesson1('abab')).toBe(true)
-// })
+test('lesson1', () => {
+    expect(lesson1([2, 4, 5, 1, 3])).toEqual([1, 2, 3, 4, 5])
+})
+
+test('lesson1', () => {
+    expect(lesson1([44, 23, 33, 21, 25, 9, 3, 4, 1])).toEqual([1, 3, 4, 9, 21, 23, 25, 33, 44])
+})
