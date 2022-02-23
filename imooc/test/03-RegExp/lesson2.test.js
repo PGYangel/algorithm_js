56d9990208b9992f3430c3859e4761d32401e74e
叶荣 <rong.ye@patpat.com>
2022-02-23 17:08:23 +0800

    正则表达式匹配

--- imooc/test/03-RegExp/lesson2.test.js
+++ imooc/test/03-RegExp/lesson2.test.js
@@ -1,5 +1,17 @@
 import lesson2 from '../../code/03-RegExp/lesson2'
 
-// test('lesson2', () => {
-//     expect(lesson2('abab')).toBe(true)
-// })
+test('lesson2', () => {
+    expect(lesson2('mississippi', 'mis*is*p*.')).toBe(false)
+})
+
+test('lesson2', () => {
+    expect(lesson2('aaa', 'caa*')).toBe(false)
+})
+
+test('lesson2', () => {
+    expect(lesson2('aaabc', 'a*bc')).toBe(true)
+})
+
+test('lesson2', () => {
+    expect(lesson2('aaabc', 'a*.*')).toBe(true)
+})
