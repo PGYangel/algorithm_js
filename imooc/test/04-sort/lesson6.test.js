55f5d014508b0beacc3bbc64e2bb9c8f226e63ed
PGY_angel <241761025@qq.com>
2022-02-26 20:27:29 +0800

    缺失的第一个正数

---
+++ imooc/test/04-sort/lesson6.test.js
@@ -0,0 +1,9 @@
+import lesson6 from '../../code/04-sort/lesson6'
+
+// test('lesson6', () => {
+//     expect(lesson6([3, 2, 1, 5, 6, 4], 2)).toBe(5)
+// })
+
+// test('lesson6', () => {
+//     expect(lesson6([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)).toBe(4)
+// })
