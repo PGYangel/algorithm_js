5ae14e793e532eb24ca3b101c0b0ed582ad7c56b
PGY_angel <241761025@qq.com>
2022-03-06 10:22:10 +0800

    任务调度器

---
+++ imooc/test/07-queue/lesson2.test.js
@@ -0,0 +1,10 @@
+import MyCircularQueue from '../../code/07-queue/lesson2'
+
+// test('lesson2', () => {
+//     let queue = new MyCircularQueue(4)
+//     queue.enQueue(2)
+//     expect(queue.isFull()).toBe(false)
+//     expect(queue.enQueue(1)).toBe(true)
+//     expect(queue.Front()).toBe(2)
+//     expect(queue.Rear()).toBe(1)
+// })
